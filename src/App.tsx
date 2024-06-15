47441bc1a82417c0a5428d37cb619f88bfe0b5ab
YKDman <dbsrudejr300@gmail.com>
2024-06-15 10:52:32 +0900

    project start

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="main flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
