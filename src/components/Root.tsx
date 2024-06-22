3981ca76886dbcfa977101f06c6d2419155b2bf7
YKDman <dbsrudejr300@gmail.com>
2024-06-22 22:56:32 +0900

    feature : Root tailwind update

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -2,13 +2,13 @@ import MainNavigation from "./MainNavigation";
 import { Outlet } from "react-router-dom";
 
 const className =
-  "h-full flex justify-center items-baseline fixed top-[96px] w-full md:h-max";
+  "flex justify-center items-baseline pt-[96px] w-full h-full md:h-auto overflow-auto";
 
 function Root() {
   return (
     <>
       <MainNavigation />
-      <main className="flex justify-center items-baseline pt-[96px] w-full h-full md:h-auto overflow-auto">
+      <main className="grid place-items-center pt-[96px] w-full h-full md:h-auto overflow-auto">
         <Outlet />
       </main>
     </>
