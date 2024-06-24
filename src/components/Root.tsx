9dc9f5b422c7d44b3cdecf4e9237b5d012f7af1b
YKDman <dbsrudejr300@gmail.com>
2024-06-24 16:20:23 +0900

    index.html lang to 'ko'

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -4,11 +4,14 @@ import { Outlet } from "react-router-dom";
 const className =
   "flex justify-center items-baseline pt-[96px] w-full h-full md:h-auto overflow-auto";
 
+const mainClass =
+  "grid place-items-center pt-[96px] w-full h-full md:h-auto overflow-auto";
+
 function Root() {
   return (
     <>
       <MainNavigation />
-      <main className="grid place-items-center pt-[96px] w-full h-full md:h-auto overflow-auto">
+      <main className="grid place-items-center pt-[96px] w-screen h-screen overflow-auto">
         <Outlet />
       </main>
     </>
